3d2501434158b8b3efb9638658a9a16169d1fe75
Supreeth Kumar <supreeth@xtranssolutions.com>
2019-01-22 12:17:30 +0530

    Test program for buzzer interface

---
+++ test/buzzer.py
@@ -0,0 +1,19 @@
+# @Auth Xtrans Solutions Pvt. Ltd.
+# Program to test Buzzer
+
+import time
+import RPi.GPIO as gpio
+
+gpio.setwarnings(False)
+gpio.setmode(gpio.BOARD)
+gpio.setup(28,gpio.OUT)
+
+try:
+    while True:
+        	gpio.output(28,0)
+        	time.sleep(.3)
+        	gpio.output(28,1)
+       		time.sleep(.3)
+except KeyboardInterrupt:
+            gpio.cleanup()
+             exit
