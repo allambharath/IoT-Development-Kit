ab49a339169b99a60a8836ba8707e6dcd3ba8c20
Supreeth Kumar <supreeth@xtranssolutions.com>
2019-01-23 18:11:47 +0530

    Gas sensor with imported library

--- test/gas.py
+++ test/gas.py
@@ -1,6 +1,9 @@
 # @Auth Xtrans Solutions Pvt. Ltd.
 # @Supreeth
 # Program to test light sensor
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+import time
 
 SPI_PORT   = 0
 SPI_DEVICE = 0
@@ -8,5 +11,6 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
  
 while True :
 	value = mcp.read_adc(0)
-	print(“Gas Value : “)
-	print(value);
+	print("Gas Value : ")
+	print(value)
+	time.sleep(2)
