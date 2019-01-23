6e56432665c569f03b2adc160cf2655135f065db
Supreeth Kumar <supreeth@xtranssolutions.com>
2019-01-23 19:27:13 +0530

    soil moisture testing code

---
+++ test/soil_moisture.py
@@ -0,0 +1,16 @@
+# @Auth Xtrans Solutions Pvt. Ltd.
+# @Supreeth
+# Program to test light sensor
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+import time
+
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+ 
+while True :
+	value = mcp.read_adc(0)
+	print("Moisture Value : ")
+	print(value)
+	time.sleep(2)
