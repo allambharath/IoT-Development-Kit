b47874379e583db2c743540fb2a8ea3e854b821f
Bharath kumar <34125323+allambharath@users.noreply.github.com>
2019-01-30 15:17:38 +0530

    Update LCD.ino

--- test/Arduino/LCD/LCD.ino
+++ test/Arduino/LCD/LCD.ino
@@ -1,50 +1,31 @@
 /*
   LiquidCrystal Library - display() and noDisplay()
-
  Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
  library works with all LCD displays that are compatible with the
  Hitachi HD44780 driver. There are many of them out there, and you
  can usually tell them by the 16-pin interface.
-
  This sketch prints "Hello World!" to the LCD and uses the
  display() and noDisplay() functions to turn on and off
  the display.
-
  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
+ * LCD RS pin to digital pin 16
+ * LCD Enable pin to digital pin 17
+ * LCD D4 pin to digital pin 13
+ * LCD D5 pin to digital pin 12
+ * LCD D6 pin to digital pin 11
+ * LCD D7 pin to digital pin 10
  * LCD R/W pin to ground
  * 10K resistor:
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalDisplay
-
+ Connect 10 pin FRC cable from CN2 to CN6
 */
-
 // include the library code:
 #include <LiquidCrystal.h>
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const int rs = 20, en = 21, d4 = 19, d5 = 18, d6 = 17, d7 = 16;
+const int rs = 16, en = 17, d4 =13, d5 = 12, d6 = 11, d7 = 10;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
