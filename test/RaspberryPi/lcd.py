c14ec2ef5eea1345e6df0a6a41ac384f5b76555c
Bharath kumar <34125323+allambharath@users.noreply.github.com>
2019-02-01 07:01:37 +0530

    Update lcd.py

--- test/RaspberryPi/lcd.py
+++ test/RaspberryPi/lcd.py
@@ -1,52 +1,141 @@
-#!/usr/bin/python
-# Example using a character LCD connected to a Raspberry Pi
-from time import sleep, strftime
-import Adafruit_CharLCD as LCD
-from datetime import datetime
-#we are calling for DATE
-
-# Raspberry Pi pin setup
-lcd_rs = 19  #Board 35
-lcd_en = 26  #Board 37
-lcd_d4 = 13  #Board 33
-lcd_d5 = 4   #Board 7
-lcd_d6 = 6  #Board 31
-lcd_d7 = 5  #Board 29
-lcd_backlight = 2
-
-# Define LCD column and row size for 16x2 LCD.
-lcd_columns = 16
-lcd_rows = 2
-
-#ds1307 = SDL_DS1307.SDL_DS1307(1, 0x68)  #entering I2c address, which we recorded previously
-#ds1307.write_now()
-
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
-
-
-
-lcd.message('Hello\nXTRANS!')
-# Wait 5 seconds
-
-sleep(5.0)
-lcd.clear()
-#text = raw_input("Type Something to be displayed: ")
-#lcd.message(text)
-
-# Wait 5 seconds
-##sleep(5.0)
-##lcd.clear()
-##lcd.message('Goodbye\nWorld!')
-##sleep(5.0)
-while True:
-    lcd.clear()
-    lcd.message(datetime.now().strftime('%b %d  %H:%M:%S\n'))
-    sleep(1)
-            
-##    lcd.message(":")
-##    lcd.message(ds1307._read_minutes())
-##    lcd.message(":")
-##    lcd.message(ds1307._read_seconds())
-##    lcd.message(":")
-##    time.sleep(1.0)
-#lcd.clear()
+# 16: LCD Backlight GND
+ 
+#import
+import RPi.GPIO as GPIO
+import time
+ 
+# Define GPIO to LCD mapping
+LCD_RS = 22
+LCD_E  = 18
+LCD_D4 = 11
+LCD_D5 = 9
+LCD_D6 = 10
+LCD_D7 = 8
+ 
+# Define some device constants
+LCD_WIDTH = 16    # Maximum characters per line
+LCD_CHR = True
+LCD_CMD = False
+ 
+LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+ 
+# Timing constants
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+ 
+def main():
+  # Main program block
+  GPIO.setwarnings(False)
+  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
+  GPIO.setup(LCD_E, GPIO.OUT)  # E
+  GPIO.setup(LCD_RS, GPIO.OUT) # RS
+  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
+  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
+  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
+  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+ 
+  # Initialise display
+  lcd_init()
+ 
+  while True:
+ 
+    # Send some test
+    lcd_string("Rasbperry Pi",LCD_LINE_1)
+    lcd_string("16x2 LCD Test",LCD_LINE_2)
+ 
+    time.sleep(3) # 3 second delay
+ 
+    # Send some text
+    lcd_string("1234567890123456",LCD_LINE_1)
+    lcd_string("abcdefghijklmnop",LCD_LINE_2)
+ 
+    time.sleep(3) # 3 second delay
+ 
+    # Send some text
+    lcd_string("RaspberryPi-spy",LCD_LINE_1)
+    lcd_string(".co.uk",LCD_LINE_2)
+ 
+    time.sleep(3)
+ 
+    
+def lcd_init():
+  # Initialise display
+  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
+  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+  time.sleep(E_DELAY)
+ 
+def lcd_byte(bits, mode):
+  # Send byte to data pins
+  # bits = data
+  # mode = True  for character
+  #        False for command
+ 
+  GPIO.output(LCD_RS, mode) # RS
+ 
+  # High bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x10==0x10:
+    GPIO.output(LCD_D4, True)
+  if bits&0x20==0x20:
+    GPIO.output(LCD_D5, True)
+  if bits&0x40==0x40:
+    GPIO.output(LCD_D6, True)
+  if bits&0x80==0x80:
+    GPIO.output(LCD_D7, True)
+ 
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+ 
+  # Low bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x01==0x01:
+    GPIO.output(LCD_D4, True)
+  if bits&0x02==0x02:
+    GPIO.output(LCD_D5, True)
+  if bits&0x04==0x04:
+    GPIO.output(LCD_D6, True)
+  if bits&0x08==0x08:
+    GPIO.output(LCD_D7, True)
+ 
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+ 
+def lcd_toggle_enable():
+  # Toggle enable
+  time.sleep(E_DELAY)
+  GPIO.output(LCD_E, True)
+  time.sleep(E_PULSE)
+  GPIO.output(LCD_E, False)
+  time.sleep(E_DELAY)
+ 
+def lcd_string(message,line):
+  # Send string to display
+ 
+  message = message.ljust(LCD_WIDTH," ")
+ 
+  lcd_byte(line, LCD_CMD)
+ 
+  for i in range(LCD_WIDTH):
+    lcd_byte(ord(message[i]),LCD_CHR)
+ 
+if __name__ == '__main__':
+ 
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
+  finally:
+    lcd_byte(0x01, LCD_CMD)
+    lcd_string("Goodbye!",LCD_LINE_1)
+    GPIO.cleanup()
