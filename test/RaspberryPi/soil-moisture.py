c5e7dc628f92c67662e3df730ccb94065fbcccc6
Bharath kumar <34125323+allambharath@users.noreply.github.com>
2019-02-01 07:45:53 +0530

    Update soil-moisture.py

--- test/RaspberryPi/soil-moisture.py
+++ test/RaspberryPi/soil-moisture.py
@@ -1,6 +1,6 @@
 # @Auth Xtrans Solutions Pvt. Ltd.
 # @Supreeth
-# Program to test light sensor
+# Program to test Moisture Sensor
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 import time
@@ -10,7 +10,7 @@ SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
  
 while True :
-	value = mcp.read_adc(0)
+	value = mcp.read_adc(1)
 	print("Moisture Value : ")
 	print(value)
 	time.sleep(2)
